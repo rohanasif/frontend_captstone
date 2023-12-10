186e32d56ee4ea63dc5945377fa2a69b1e5905a8
rohanasif1990 <rohanasif1990@gmail.com>
2023-12-10 23:10:14 +0500

    user/login flow complete, implementation left

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,11 @@
 import axios from "axios";
-import BASEURL from "../constants";
+import BASEURL, {
+  LOGIN_SUCCESS,
+  LOGOUT_SUCCESS,
+  SIGNUP_ERROR,
+  SIGNUP_SUCCESS,
+} from "../constants";
+
 export const getUsers = () => async () => {
   try {
     const response = await axios.get(`${BASEURL}/users`);
@@ -12,6 +18,17 @@ export const getUsers = () => async () => {
 
 export const signup = (user) => async (dispatch) => {
   try {
+    const allUsers = await dispatch(getUsers());
+    if (
+      allUsers.length === 0 ||
+      !allUsers.find((u) => u.email === user.email)
+    ) {
+      const response = await axios.post(`${BASEURL}/users`, user);
+      dispatch({ type: SIGNUP_SUCCESS, payload: response.data });
+      alert("Registration successful!");
+    } else {
+      dispatch({ type: SIGNUP_ERROR, payload: "User already registered!" });
+    }
   } catch (e) {
     console.error(e);
   }
@@ -19,6 +36,31 @@ export const signup = (user) => async (dispatch) => {
 
 export const login = (user) => async (dispatch) => {
   try {
+    const allUsers = await dispatch(getUsers());
+    const userLoggingIn = allUsers.find(
+      (u) => u.password === user.password && u.email === user.email
+    );
+    if (userLoggingIn) {
+      const response = await axios.patch(
+        `${BASEURL}/users/${userLoggingIn.id}`,
+        { isLoggedin: true }
+      );
+      dispatch({ type: LOGIN_SUCCESS, payload: response.data });
+      alert("Login successful!");
+    } else {
+      dispatch({ type: SIGNUP_ERROR, payload: "Invalid login credentials!" });
+    }
+  } catch (e) {
+    console.error(e);
+  }
+};
+
+export const getLoggedUser = () => async () => {
+  try {
+    const response = await axios.get(`${BASEURL}/users?isLoggedin=true`);
+    const userArr = response.data;
+    const user = userArr[0];
+    return user;
   } catch (e) {
     console.error(e);
   }
@@ -26,6 +68,10 @@ export const login = (user) => async (dispatch) => {
 
 export const logout = (user) => async (dispatch) => {
   try {
+    const response = await axios.patch(`${BASEURL}/users/${user.id}`, {
+      isLoggedin: false,
+    });
+    dispatch({ type: LOGOUT_SUCCESS, payload: response.data });
   } catch (e) {
     console.error(e);
   }
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,41 @@
-const initialState = { users: [], message: "", dishes: [] };
+import {
+  LOGIN_ERROR,
+  LOGIN_SUCCESS,
+  SIGNUP_ERROR,
+  SIGNUP_SUCCESS,
+  LOGOUT_SUCCESS,
+} from "../constants";
+
+const initialState = { users: [], dishes: [] };
 
 const rootReducer = (state = initialState, action) => {
   switch (action.type) {
+    case SIGNUP_SUCCESS:
+      return { ...state, users: [...state.users, action.payload] };
+    case SIGNUP_ERROR:
+      return {
+        ...state,
+        message: action.payload,
+      };
+    case LOGIN_SUCCESS:
+      return {
+        ...state,
+        users: state.users.map((user) =>
+          user.id === action.payload.id ? { ...user, isLoggedin: true } : user
+        ),
+      };
+    case LOGIN_ERROR:
+      return {
+        ...state,
+        message: action.payload,
+      };
+    case LOGOUT_SUCCESS:
+      return {
+        ...state,
+        users: state.users.map((user) =>
+          user.id === action.payload.id ? { ...user, isLoggedin: false } : user
+        ),
+      };
     default:
       return state;
   }
