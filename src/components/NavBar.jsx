71870d3be112ec13fac4d3ddb4f4a88e04ae9969
rohanasif1990 <rohanasif1990@gmail.com>
2023-12-10 20:58:41 +0500

    Installed fontawesome

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -17,23 +17,15 @@ const NavBar = () => {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link>
-              <Link></Link>
+            <Nav.Link as={Link} to="/">
+              Home
             </Nav.Link>
-            <Nav.Link>
-              <Link to="/about">About</Link>
+            <Nav.Link as={Link} to="/start">
+              Login/Signup
             </Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
+            <Nav.Link as={Link} to="/cart">
+              
+            </Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
--- src/routes/Homepage.jsx
+++ src/routes/Homepage.jsx
@@ -1,5 +1,10 @@
+import NavBar from "../components/NavBar";
 const Homepage = () => {
-  return <div>Homepage</div>;
+  return (
+    <div>
+      <NavBar />
+    </div>
+  );
 };
 
 export default Homepage;
